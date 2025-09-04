24c1041d744ca6d4dfe0eaf09d480b3c0785df17
seunghyonpark <creath.park@gmail.com>
2025-09-04 23:19:12 +0900

    feat: Implement POST endpoint to retrieve user by Telegram ID

---
+++ src/app/api/user/getUserByTelegramId/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import {
+	getPayUserByTelegramId,
+} from '@lib/api/user';
+
+
+
+export async function POST(request: NextRequest) {
+
+  const body = await request.json();
+
+  const {
+    telegramId,
+  } = body;
+
+
+  //console.log("walletAddress", walletAddress);
+
+
+  const result = await getPayUserByTelegramId(
+    telegramId,
+  );
+
+  console.log("getUserByTelegramId result", result);
+
+ 
+  return NextResponse.json({
+
+    result,
+    
+  });
+  
+}
