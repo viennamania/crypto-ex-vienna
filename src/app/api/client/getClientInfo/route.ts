39c00b6c0a258279bab9403a1843ffb416ba8db6
seunghyonpark <creath.park@gmail.com>
2025-09-07 18:04:36 +0900

    fix: correct object property assignment for chain and clientId in getClientInfo route

--- src/app/api/client/getClientInfo/route.ts
+++ src/app/api/client/getClientInfo/route.ts
@@ -4,12 +4,15 @@ import { NextResponse, type NextRequest } from "next/server";
 import { chain } from "@/app/config/contractAddresses";
 
 
+
+
 export async function POST(request: NextRequest) {
 
 
 
   const result = {
-    chain,
+    chain: chain,
+    clientId: process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID || '',
   };
 
   return NextResponse.json({
