e737b7ac216940cf3a8fcb41a2456ec6f3f01fa1
seunghyonpark <creath.park@gmail.com>
2025-09-09 10:04:27 +0900

    fix: update copyright text for consistency across multiple pages

--- src/app/[lang]/[center]/daily-close/page.tsx
+++ src/app/[lang]/[center]/daily-close/page.tsx
@@ -1813,7 +1813,7 @@ export default function Index({ params }: any) {
             
             <div className="w-full flex flex-col items-center justify-center gap-4 p-4 bg-white shadow-md rounded-lg mt-5">
               <div className="text-sm text-zinc-600">
-                © 2024 Good Tether All rights reserved.
+                © 2024 GoodTether All rights reserved.
               </div>
               <div className="text-sm text-zinc-600">
                 <a href={`/${params.lang}/terms-of-service`} className="text-blue-500 hover:underline">
