bd245f10a1aa3e44865f294df2138e4577f43549
seunghyonpark <creath.park@gmail.com>
2026-01-12 11:58:30 +0900

    feat(buyorder): update layout for processing buy orders and add TID display with copy functionality

--- src/app/[lang]/seller/buyorder/page.tsx
+++ src/app/[lang]/seller/buyorder/page.tsx
@@ -4681,8 +4681,8 @@ const fetchBuyOrders = async () => {
                 {/* array of processingBuyOrders store logos */}
                 {processingBuyOrders.length > 0 && (
                 <div className="w-full
-                  grid grid-cols-2
-                  xl:flex flex-row items-center justify-end gap-2">
+                  flex flex-col
+                  xl:flex-row items-center justify-end gap-2">
                   
                   {/* background dark colors */}
                   {processingBuyOrders.slice(0, 5).map((order: BuyOrder, index: number) => (
@@ -6236,6 +6236,30 @@ const fetchBuyOrders = async () => {
                                 </button>
                             </span>
                             */}
+
+                            {/* /icon-trade.png */}
+                            {/* TID */}
+                            <div className="w-full flex flex-row items-center justify-start gap-2">
+                              <Image
+                                src="/icon-trade.png"
+                                alt="In Trade"
+                                width={50}
+                                height={50}
+                                className="w-16 h-16 object-contain"
+                              />
+                              <span className="text-sm">
+                                TID: #<button
+                                    className="text-sm text-zinc-600 underline"
+                                    style={{ fontFamily: 'monospace' }}
+                                    onClick={() => {
+                                      navigator.clipboard.writeText(seller.seller?.buyOrder?.tradeId);
+                                      toast.success('TID가 복사되었습니다.');
+                                    } }
+                                  >
+                                    {seller.seller?.buyOrder?.tradeId}
+                                  </button>
+                              </span>
+                            </div>
 
                             <div className="flex flex-row items-center gap-2
                               bg-blue-500 text-white px-3 py-1 rounded-lg">
