4b290245ad65c2c05136feb7be910f07671ccf8e
seunghyonpark <creath.park@gmail.com>
2026-01-15 15:52:50 +0900

    fix(buyorder): add labels and instructions for USDT purchase input fields

--- src/app/[lang]/buyer/buyorder/page.tsx
+++ src/app/[lang]/buyer/buyorder/page.tsx
@@ -5538,6 +5538,13 @@ const fetchBuyOrders = async () => {
 
             </div>
 
+
+
+            {/* 지정가로 구매주문하기 */}
+            
+
+
+
             {sellersBalance.length > 0 && (
               <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
 
@@ -7062,6 +7069,10 @@ const fetchBuyOrders = async () => {
                                   `}
                                   disabled={!address || !user?.buyer?.bankInfo || buyOrderingPrivateSaleArray[index]}
                                 />
+                                {/* 구해할 USDT 수량을 입력해주세요. */}
+                                <span className="text-sm text-slate-400">
+                                  구해할 USDT 수량을 입력해주세요.
+                                </span>
                                 <button
                                   onClick={() => {
                                     buyOrderPrivateSale(
@@ -7085,7 +7096,7 @@ const fetchBuyOrders = async () => {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                       <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
                                     </svg>
-                                    구매하기
+                                    USDT 구매하기
                                   </span>
                                 </button>
                               </div>
